b3b305916f258c57af30e10caddb428f395cb7cf
Samuel Laferriere <samlaf92@gmail.com>
2023-07-30 18:31:04 -0400

    added comment explaining v,r,s for ecrecover

--- contracts/src/Ecrecover.sol
+++ contracts/src/Ecrecover.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-// Following https://soliditydeveloper.com/ecrecover
+// See https://soliditydeveloper.com/ecrecover
+// and https://github.com/ethereumbook/ethereumbook/blob/develop/06transactions.asciidoc#how-digital-signatures-work
+// (r,s) is the signature. 
+// v is used to uniquely recover the signer's address (from 2 possible choices)
+//    see https://github.com/ethereumbook/ethereumbook/blob/develop/06transactions.asciidoc#the-signature-prefix-value-v-and-public-key-recovery
 contract Ecrecover {
     function recoverSignerFromSignature(
         uint8 v,
